900229aebd94d8e96392779cf4049206fa10a2de
Kong Yang <kyang1212@gmail.com>
2018-02-01 22:50:17 -0500

    Initial Commit

---
+++ FileMover/FileMover/FileMover.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMover
+{
+    public class FileMover
+    {
+        public static string[] GetFiles()
+        {
+            string[] files = Directory.GetFiles($"{Environment.GetEnvironmentVariable("USERPROFILE")}/Documents/Test/","*.txt");
+            return files;
+        }
+
+        public static void MoveToTest2(string filepath)
+        {
+            int lastIndex = filepath.LastIndexOf("/");
+            string fileName = filepath.Substring(lastIndex + 1);
+            Directory.Move(filepath, $"{Environment.GetEnvironmentVariable("USERPROFILE")}/Documents/Test2/{fileName}");
+        }
+
+        public static void Move(string sourceFilePath, string destinationFilePath)
+        {
+            int lastIndex = sourceFilePath.LastIndexOf("/");
+            if(lastIndex == sourceFilePath.Length - 1)
+            {
+                throw new ArgumentException("There is not a valid item to move.");
+            }
+            if (!destinationFilePath.Last().Equals("/"))
+            {
+                destinationFilePath = destinationFilePath + "/";
+            }
+            string fileName = sourceFilePath.Substring(lastIndex + 1);
+            Directory.Move(sourceFilePath, destinationFilePath+ fileName);
+        }
+    }
+}
---
+++ FileMover/FileMoverTests/FileMoverTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileMover;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileMover.Tests
+{
+    [TestClass()]
+    public class FileMoverTests
+    {
+        [TestMethod()]
+        public void GetFilesTest()
+        {
+            List<string> files = FileMover.GetFiles().ToList();
+            foreach(var item in files)
+            {
+                Console.WriteLine(item);
+                FileMover.MoveToTest2(item);
+            }
+            Assert.AreNotEqual(files.Count, 0);
+        }
+    }
+}
